52d8546f49f708eac8450831ed1d92e3153130e0
sleek-ke <mboacodes@gmail.com>
2025-01-16 22:02:38 +0300

    Build automation

--- build.sh
+++ build.sh
@@ -2,15 +2,37 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
+# Install required packages
 pip install -r requirements.txt
 
-# Convert static asset files
+# Collect static files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
-
+# Apply database migrations
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py createsuperuser
+export DJANGO_SUPERUSER_USERNAME=junction
+export DJANGO_SUPERUSER_EMAIL=junction@gmail.com
+export DJANGO_SUPERUSER_PASSWORD=Junction@123
+
+# Create superuser if it doesn't exist
+python <<EOF
+import os
+from decouple import config
+
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME", "junction")
+email = os.getenv"DJANGO_SUPERUSER_EMAIL", "junction@gmail.com")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD", "Junction@123")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username=username, email=email, password=password)
+    print(f"Superuser '{username}' created successfully.")
+else:
+    print(f"Superuser '{username}' already exists.")
+EOF
